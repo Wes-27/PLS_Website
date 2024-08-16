fadf47bd72d7641abe3435d6d41d31181a284769
Wes James <133700495+Wes-27@users.noreply.github.com>
2024-08-16 17:44:25 +0000

    Testing database connection

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
+import os
+import sys
 from flask import Flask, render_template
+import getpass
+import oracledb
+
+un = "ADMIN"
+cs = "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.us-phoenix-1.oraclecloud.com))(connect_data=(service_name=g4a560c102d9091_tipjfxu4a4ibrwrv_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))"
+pw = getpass.getpass(f'Enter password for {un}@{cs}: ')
+
+with oracledb.connect(user=un, password=pw, dsn=cs) as connection:
+  with connection.cursor() as cursor:
+      sql = """select sysdate from dual"""
+      for r in cursor.execute(sql):
+          print(r)
+
 
 app = Flask(__name__)
+
 
 
 @app.route("/")
